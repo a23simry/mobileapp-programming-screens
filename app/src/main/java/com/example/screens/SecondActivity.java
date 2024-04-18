10601c8e9b53e388ab44c294ba970b399dda12b2
SimonRydberg99 <simonrydberg@hotmail.com>
2024-04-18 23:16:41 +0200

    Added textview widget to second activity which shows data from mainactivity onclick

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -1,21 +1,28 @@
 package com.example.screens;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        text = findViewById(R.id.textViewInfo);
+
         Bundle extras = getIntent().getExtras();
         if (extras != null){
-            String name = extras.getString("name");
-            int number = extras.getInt("number");
+            String name = extras.getString("Who");
+            int number = extras.getInt("Age");
             String sleep = extras.getString("Need sleep?");
+
+            text.setText("Name: " + name + " Age: " + number + " Need sleep: " + sleep);
 
         }
 
